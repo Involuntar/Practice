36b9afa5bb3295c5102cb2036c808204dec2c7c3
Ilya <ilya.nevolin.2004@mail.ru>
2024-08-15 19:39:24 +0500

    Привёл к виду форму информации о марафоне

--- Practice/Forms/MarathonInfo.Designer.cs
+++ Practice/Forms/MarathonInfo.Designer.cs
@@ -29,11 +29,236 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.LBL_MarathonInfo = new System.Windows.Forms.Label();
+            this.LBL_MarathonSkillsInfo = new System.Windows.Forms.Label();
+            this.LBL_MoreInfo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LBL_MarathonName = new System.Windows.Forms.Label();
+            this.BTN_Back = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LBL_TimeToStart = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LBL_MoreMoreInfo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LBL_MarathonInfo
+            // 
+            this.LBL_MarathonInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LBL_MarathonInfo.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Bold);
+            this.LBL_MarathonInfo.Location = new System.Drawing.Point(0, 79);
+            this.LBL_MarathonInfo.Name = "LBL_MarathonInfo";
+            this.LBL_MarathonInfo.Size = new System.Drawing.Size(800, 49);
+            this.LBL_MarathonInfo.TabIndex = 0;
+            this.LBL_MarathonInfo.Text = "Информация о Marathon Skills 2024";
+            this.LBL_MarathonInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LBL_MarathonSkillsInfo
+            // 
+            this.LBL_MarathonSkillsInfo.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.LBL_MarathonSkillsInfo.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.LBL_MarathonSkillsInfo.Location = new System.Drawing.Point(371, 157);
+            this.LBL_MarathonSkillsInfo.Name = "LBL_MarathonSkillsInfo";
+            this.LBL_MarathonSkillsInfo.Size = new System.Drawing.Size(417, 165);
+            this.LBL_MarathonSkillsInfo.TabIndex = 1;
+            this.LBL_MarathonSkillsInfo.Text = "Информация о Marathon Skills.";
+            // 
+            // LBL_MoreInfo
+            // 
+            this.LBL_MoreInfo.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.LBL_MoreInfo.Location = new System.Drawing.Point(371, 322);
+            this.LBL_MoreInfo.Name = "LBL_MoreInfo";
+            this.LBL_MoreInfo.Size = new System.Drawing.Size(417, 75);
+            this.LBL_MoreInfo.TabIndex = 2;
+            this.LBL_MoreInfo.Text = "Доп. сведения";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(164, 165);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(156, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 246);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(155, 78);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(164, 246);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(156, 78);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(144)))), ((int)(((byte)(62)))));
+            this.panel1.Controls.Add(this.LBL_MarathonName);
+            this.panel1.Controls.Add(this.BTN_Back);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 79);
+            this.panel1.TabIndex = 7;
+            // 
+            // LBL_MarathonName
+            // 
+            this.LBL_MarathonName.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LBL_MarathonName.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold);
+            this.LBL_MarathonName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LBL_MarathonName.Location = new System.Drawing.Point(131, 0);
+            this.LBL_MarathonName.Name = "LBL_MarathonName";
+            this.LBL_MarathonName.Size = new System.Drawing.Size(481, 79);
+            this.LBL_MarathonName.TabIndex = 23;
+            this.LBL_MarathonName.Text = "Marathon Skills 2024";
+            this.LBL_MarathonName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BTN_Back
+            // 
+            this.BTN_Back.BackColor = System.Drawing.Color.DarkGray;
+            this.BTN_Back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Back.Dock = System.Windows.Forms.DockStyle.Left;
+            this.BTN_Back.Location = new System.Drawing.Point(0, 0);
+            this.BTN_Back.Margin = new System.Windows.Forms.Padding(5);
+            this.BTN_Back.Name = "BTN_Back";
+            this.BTN_Back.Size = new System.Drawing.Size(131, 79);
+            this.BTN_Back.TabIndex = 0;
+            this.BTN_Back.Text = "Назад";
+            this.BTN_Back.UseVisualStyleBackColor = false;
+            this.BTN_Back.Click += new System.EventHandler(this.BTN_Back_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(144)))), ((int)(((byte)(62)))));
+            this.panel2.Controls.Add(this.LBL_TimeToStart);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 517);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 46);
+            this.panel2.TabIndex = 25;
+            // 
+            // LBL_TimeToStart
+            // 
+            this.LBL_TimeToStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(144)))), ((int)(((byte)(62)))));
+            this.LBL_TimeToStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LBL_TimeToStart.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_TimeToStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LBL_TimeToStart.Location = new System.Drawing.Point(0, 0);
+            this.LBL_TimeToStart.Name = "LBL_TimeToStart";
+            this.LBL_TimeToStart.Size = new System.Drawing.Size(800, 46);
+            this.LBL_TimeToStart.TabIndex = 23;
+            this.LBL_TimeToStart.Text = "Время до";
+            this.LBL_TimeToStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LBL_TimeToStart.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // LBL_MoreMoreInfo
+            // 
+            this.LBL_MoreMoreInfo.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.LBL_MoreMoreInfo.Location = new System.Drawing.Point(368, 403);
+            this.LBL_MoreMoreInfo.Name = "LBL_MoreMoreInfo";
+            this.LBL_MoreMoreInfo.Size = new System.Drawing.Size(420, 78);
+            this.LBL_MoreMoreInfo.TabIndex = 26;
+            this.LBL_MoreMoreInfo.Text = "Доп. доп. сведения";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 157);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(323, 327);
+            this.tableLayoutPanel1.TabIndex = 27;
+            // 
+            // MarathonInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 563);
+            this.Controls.Add(this.LBL_MarathonInfo);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.LBL_MoreMoreInfo);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LBL_MoreInfo);
+            this.Controls.Add(this.LBL_MarathonSkillsInfo);
+            this.Name = "MarathonInfo";
             this.Text = "MarathonInfo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LBL_MarathonInfo;
+        private System.Windows.Forms.Label LBL_MarathonSkillsInfo;
+        private System.Windows.Forms.Label LBL_MoreInfo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LBL_MarathonName;
+        private System.Windows.Forms.Button BTN_Back;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label LBL_TimeToStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label LBL_MoreMoreInfo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
