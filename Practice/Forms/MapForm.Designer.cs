5cdf9ae13eccb3d442ba7be9bac39ab1b6d5bd9e
Ilya <ilya.nevolin.2004@mail.ru>
2024-08-15 21:56:04 +0500

    создал форму карты

---
+++ Practice/Forms/MapForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Practice.Forms
+{
+    partial class MapForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LBL_MarathonName = new System.Windows.Forms.Label();
+            this.BTN_Back = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(144)))), ((int)(((byte)(62)))));
+            this.panel1.Controls.Add(this.LBL_MarathonName);
+            this.panel1.Controls.Add(this.BTN_Back);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 79);
+            this.panel1.TabIndex = 9;
+            // 
+            // LBL_MarathonName
+            // 
+            this.LBL_MarathonName.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LBL_MarathonName.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold);
+            this.LBL_MarathonName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LBL_MarathonName.Location = new System.Drawing.Point(131, 0);
+            this.LBL_MarathonName.Name = "LBL_MarathonName";
+            this.LBL_MarathonName.Size = new System.Drawing.Size(481, 79);
+            this.LBL_MarathonName.TabIndex = 23;
+            this.LBL_MarathonName.Text = "Marathon Skills 2024";
+            this.LBL_MarathonName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BTN_Back
+            // 
+            this.BTN_Back.BackColor = System.Drawing.Color.DarkGray;
+            this.BTN_Back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Back.Dock = System.Windows.Forms.DockStyle.Left;
+            this.BTN_Back.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold);
+            this.BTN_Back.Location = new System.Drawing.Point(0, 0);
+            this.BTN_Back.Margin = new System.Windows.Forms.Padding(5);
+            this.BTN_Back.Name = "BTN_Back";
+            this.BTN_Back.Size = new System.Drawing.Size(131, 79);
+            this.BTN_Back.TabIndex = 0;
+            this.BTN_Back.Text = "Назад";
+            this.BTN_Back.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(35, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 322);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(451, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(218, 236);
+            this.panel2.TabIndex = 11;
+            this.panel2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(191, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // MapForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MapForm";
+            this.Text = "MapForm";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LBL_MarathonName;
+        private System.Windows.Forms.Button BTN_Back;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
